09f1fe4b7cd65ea6496212ac533f098d332ef96a
ANA SILVA <ana.silva2010@fatec.sp.gov.br>
2021-02-11 19:03:19 -0300

    Add ViewModels

---
+++ src/backend/DesafioQuiz.Api/DesafioQuiz.Api/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DesafioQuiz.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioQuiz.Api.ViewModels
+{
+    public class QuestionViewModel 
+    {
+        public int Id { get; set; }
+        public int MyProperty { get; set; }
+        public List<Replies> ListReplies { get; set; }
+    }
+}
+
---
+++ src/backend/DesafioQuiz.Api/DesafioQuiz.Api/ViewModels/RepliesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioQuiz.Api.ViewModels
+{
+    public class RepliesViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int QuestionId { get; set; }
+        public bool AnswerCorrect { get; set; }
+    }
+}
