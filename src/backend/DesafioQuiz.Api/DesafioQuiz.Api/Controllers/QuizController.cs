5f26fa019d6dd74956296b9144f12ed9df6fe333
ANA SILVA <ana.silva2010@fatec.sp.gov.br>
2021-02-11 21:36:28 -0300

    Implement get-questions

--- src/backend/DesafioQuiz.Api/DesafioQuiz.Api/Controllers/QuizController.cs
+++ src/backend/DesafioQuiz.Api/DesafioQuiz.Api/Controllers/QuizController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using AutoMapper;
+using DesafioQuiz.Api.ViewModels;
+using DesafioQuiz.Business.Interfaces;
+using DesafioQuiz.Model;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DesafioQuiz.Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class QuizController : Controller
     {
+        IMapper _mapper;
+        IQuestionService _questionService;
+        IRepliesService _repliesService;
+        public QuizController(IMapper mapper, IQuestionService questionService, IRepliesService repliesService)
+        {
+            _mapper = mapper;
+            _questionService = questionService;
+            _repliesService = repliesService;
+        }
+        [HttpGet("get-questions")]
+        public ActionResult GetAllQuestions()
+        {
+            var getQuestions = _questionService.GetAll();
+            var question = new List<QuestionViewModel>();
+
+
+            List<RepliesViewModel> getReplies = _mapper.Map<List<RepliesViewModel>>(_repliesService.GetAll());
+            var replies = new List<RepliesViewModel>();
+
+
+            foreach (var item in getQuestions)
+            {
+
+                foreach (var rep in getReplies.Where(x => x.QuestionId == item.Id))
+                {
+                    replies.Add(rep);
+                }
+
+                question.Add(new QuestionViewModel() { Id = item.Id, Description = item.Description, ListReplies = replies });
+
+                replies = new List<RepliesViewModel>();
+
+
+            }
+
+            return Ok(question);
+        }
     }
 }
