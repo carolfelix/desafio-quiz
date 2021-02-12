939ada3668fff0d45e4ab3388b77f2c116288d44
ANA SILVA <ana.silva2010@fatec.sp.gov.br>
2021-02-12 10:46:01 -0300

    Rename folder styles / index - Quiz

---
+++ src/frontend/desafioquizweb/src/pages/Quiz/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api.services';
+
+import './style.css';
+
+export default function Quiz({ history }) {
+  
+    return (
+        <>
+            <div className="container">
+                hello
+            </div>
+        </>
+    )
+}
